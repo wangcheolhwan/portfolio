d2c8b9cb173673261803f1bdd5b1d9fed0934b1b
wangcheolhwan <wangch678@gmail.com>
2024-03-06 11:13:47 +0900

    프로젝트 생성

--- LH/js/youtube.js
+++ LH/js/youtube.js
@@ -2,40 +2,41 @@ var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+console.log('script loaded');
 
 var players = [];
 
-function onYouTubePlayerAPIReady() {
+window.onYouTubePlayerAPIReady = function () {
   players.push(new YT.Player('player_01', {
     videoId: '1mK1MOwWcM4',
-    events: {
-      onReady: function (event) {
-      }
-    }
   }));
 
   players.push(new YT.Player('player_02', {
     videoId: 'odxnQoQFNRc',
-    events: {
-      onReady: function (event) {
-      }
-    }
   }));
 
   players.push(new YT.Player('player_03', {
     videoId: 'w5TmD_nhCiY',
-    events: {
-      onReady: function (event) {
-      }
-    }
   }));
+}
+console.log(players);
+
+function youtubeExit(event, index) {
+  
+  if (event.data === YT.PlayerState.PLAYING) {
+    players[index].stopVideo();
+  }
+}
+
+function stopVideo() {
+  player.stopVideo();
 }
 
 const stopBtnEls = document.querySelectorAll('#youtube .xi-close');
-console.log(stopBtnEls);
+console.log(stopBtnEls)
 
 stopBtnEls.forEach((stopBtnEl, index) => {
   stopBtnEl.addEventListener('click', () => {
-    players[index].stopVideo();
-  })
-})
+    youtubeExit({ data: players[index].getPlayerState() }, index);
+  });
+});
